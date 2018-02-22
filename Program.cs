8867385134b2688becfcc0479bba512943a67812
Murat Ay <muratay@telekurye.wan>
2018-02-22 18:38:56 +0300

    Find and shorten urls

--- Program.cs
+++ Program.cs
@@ -13,29 +13,94 @@ namespace ShortenUrl
     {
         static void Main(string[] args)
         {
-            string text =
-                "Konum bilginizi paylaşmak için lütfen bu adresi  https://tkaccurate.netlify.com/?wodId=8848848&processCode=111 ziyaret ediniz. İletişim ve görüşleriniz için http://www.telekurye.com.tr/";
+                       const string textWithUrl = "This text has URLs https://twitter.com/murat7ay," +
+                                       " https://murat7ay.netlify.com/#/. " +
+                                       "Extra if something comes https:// ," +
+                                       "something like twitter.com ," +
+                                       "http https ftp " +
+                                       "http://www.google.com " +
+                                       "http://foo:bar@w1.superman.com/very/long/path.html?p1=V1&p2=v2#more-details " +
+                                       "https://secured.com:443 " +
+                                       "ftp://ftp.bogus.com/~some/path/to/a/file.txt " +
+                                       "username:password@host.com/ " +
+                                       "subdomain.domain.com " +
+                                       "www.google.com.tr " +
+                                       "www.superaddress.com:8080 " +
+                                       "http://www.foo.bar/segment1/segment2/some-resource.html " +
+                                       "http://www.foo.bar/image-2.html?w=100&hI=50 " +
+                                       "ftp://ftp.foo.bar/~john/doe?w=100&h=50 " +
+                                       "http://www.foo.bar/image.jpg?height=150&widTth=100 " +
+                                       "https://www.secured.com:443/resource.html?id=6e8bc430-9c3a-11d9-9669-0800200c9a66#some-header " +
+                                       "ftp://ftp.is.co.za/rfc/rfc1808.txt " +
+                                       "http://www.ietf.org/rfc/rfc2396.txt " +
+                                       "ldap://[2001:db8::7]/c=GB?objectClass?one " +
+                                       "news:comp.infosystems.www.servers.unix " +
+                                       "tel:+1-816-555-1212 " +
+                                       "telnet://192.0.2.16:80/ " +
+                                       "urn:oasis:names:specification:docbook:dtd:xml:4.1.2 " +
+                                       "google.com.tr  " +
+                                       "https://rl.telekurye.com.tr/sorgula.aspx?wodid=00000&password=000 " +
+                                       "https://rl.telekurye.com.tr/sorgula.aspx?wodIaAbBd=00000&password=000 " +
+                                       "https://uygulama.telekurye.com.tr/SmartPol/smartPolAppointment?data=xxxx_xxxxxx " +
+                                       "https://amaksdkdaskdaskk.TELEKURYE.gov.tr/#asdadsasd/?key=asdsad%&?asdasd=1123...,,,,!!!";
+            const string textWithNoUrl =
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+                " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-            var googlApi = new UrlShortener("AIzaSyB_CuchBvjq9C2AcbSj3r3EIIm1dAIyHIA");
+            var googlApi = UrlShortener.Instance;
 
-            var linkParser = new Regex(@"\b(?:https?://|www\.)\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-            Console.WriteLine(text);
-            foreach (Match m in linkParser.Matches(text))
+            var result = googlApi.ConvertToShortenUrl(textWithUrl);
+
+            var resultnoUrl = googlApi.ConvertToShortenUrl(textWithNoUrl);
+
+            if (resultnoUrl.Item1)
             {
-                var serviceResult = googlApi.Shorten(m.Value);
-
-                if (serviceResult.Ok)
+                Console.WriteLine(resultnoUrl.Item2);
+                if (resultnoUrl.Item3 != null)
                 {
-                    Console.WriteLine(m.Value + "----" + serviceResult.Id);
-                    text = text.Replace(m.Value, serviceResult.Id);
-                }
-                else
-                {
-                    break;
+                    foreach (var i in resultnoUrl.Item3)
+                    {
+                        Console.WriteLine(i.Key + " - " + i.Value);
+                        Console.WriteLine("Statistics in all time click => " + googlApi.TrackerAllTimeShortUrlClicks(i.Value));
+                    }
                 }
             }
-            Console.WriteLine(googlApi.Tracker("https://goo.gl/L2NRzo").analytics.allTime.shortUrlClicks);
-            Console.ReadLine();
+
+            if (result.Item1)
+            {
+                Console.WriteLine(result.Item2);
+                if (result.Item3 != null)
+                {
+                    foreach (var i in result.Item3)
+                    {
+                        Console.WriteLine(i.Key + " - " + i.Value);
+                        Console.WriteLine("Statistics in all time click =>" + googlApi.TrackerAllTimeShortUrlClicks(i.Value));
+                    }
+                }
+            }
+
+            //txt file about 1 GB
+            //var linkParser = new Regex(@"(?:(?:https?|ftp|file):\/\/?)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            //using (FileStream fs = File.Open(@"C:\apideneme\fullSmsMessage.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            //using (BufferedStream bs = new BufferedStream(fs))
+            //using (StreamReader sr = new StreamReader(bs))
+            //{
+            //    string line;
+            //    while ((line = sr.ReadLine()) != null)
+            //    {
+            //        var m = linkParser.Matches(line);
+            //        if (m.Count == 0)
+            //            continue;
+            //        foreach (Match a in m)
+            //        {
+            //            googlApi.ConvertToShortenUrl(a.Value);
+            //            Console.WriteLine(a.Value);
+            //        }
+            //    }
+            //}
+
 
         }
     }
--- UrlShortener.cs
+++ UrlShortener.cs
@@ -1,112 +1,262 @@
-﻿using System.Linq;
-using RestSharp;
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
-using ShortenUrl.Models;
+using System.Text.RegularExpressions;
+using RestSharp;
 
 namespace ShortenUrl
 {
+     /// <summary>
+    /// This class has two methods
+    /// It makes if your text message has urls than convert them shorten urls like goo.gl/.... OR
+    /// It tracks your shorten Urls statistics
+    /// </summary>
     public class UrlShortener
     {
-        private readonly string _apiKey;
-        private readonly string _uri;
+        private static UrlShortener _instance;
 
-        private const string DefaultUri = "https://www.googleapis.com/urlshortener/v1/url";
+        private const string ApiKey = "AIzaSyB_CuchBvjq9C2AcbSj3r3EIIm1dAIyHIA1";
+        
+        private static readonly RestClient Client = new RestClient("https://www.googleapis.com/urlshortener/v1/url");
+        private static readonly CultureInfo culture = new CultureInfo("en-US");
 
-        public UrlShortener(string apiKey, string uri = DefaultUri)
+
+        private UrlShortener()
         {
-            _apiKey = apiKey;
-            _uri = uri;
+            
         }
 
-        public ApiResponse Shorten(string longUrl)
+        public static UrlShortener Instance
         {
-            var uri = _uri + "?key=" + _apiKey;
-            var client = new RestClient(uri);
-            var request = new RestRequest { Method = Method.POST };
-            var content = new { longUrl };
-            request.AddJsonBody(content);
-            var response = client.Execute<ApiResponse>(request);
+            get { return _instance ?? (_instance = new UrlShortener()); }
+        }
 
+        private string Shorten(string longUrl)
+        {
+            var request = new RestRequest("?key=" + ApiKey, Method.POST);
+            request.AddJsonBody(new { longUrl });
+            var response = Client.Execute<ApiResponse>(request);
+            return response.StatusCode == HttpStatusCode.OK ? response.Data.Id : null;
+        }
+        /// <summary>
+        /// All track info
+        /// </summary>
+        /// <param name="shortUrl"></param>
+        /// <returns></returns>
+        public AllTime Tracker(string shortUrl)
+        {
+            var request = new RestRequest("?shortUrl=" + shortUrl + "&projection=FULL&key=" + ApiKey, Method.GET);
+            var response = Client.Execute<ApiResponse>(request);
+            return response.StatusCode == HttpStatusCode.OK ? response.Data.analytics.allTime : null;
+        }
 
-            if (response.StatusCode == HttpStatusCode.OK)
+        /// <summary>
+        /// <para>Track your shorten urls clicks</para>
+        /// <para>All time shorten url clicks count or null if error occurs</para>
+        /// </summary>
+        /// <param name="shortUrl">Short url like goo.gl/.....</param>
+        public string TrackerAllTimeShortUrlClicks(string shortUrl)
+        {
+            var request = new RestRequest("?shortUrl=" + shortUrl + "&projection=FULL&key=" + ApiKey, Method.GET);
+            var response = Client.Execute<ApiResponse>(request);
+
+            return response.StatusCode == HttpStatusCode.OK ? response.Data.analytics.allTime.shortUrlClicks : null;
+        }
+        /// <summary>
+        /// <para>It finds the URLs in the message text and converts it into short URLs.</para>
+        /// <para>If the message has no URLs returns the message</para>
+        /// <para>(Item1=>true if messageText has urls and successfully convert to shorten or messageText has no urls, false =>messageText's urls can not convert to shorten)</para>
+        /// <para>(Item2 => message with no urls or message with shorten urls, if key is false message will be error)</para>
+        /// <para>(Item3 => List of key value pair Key is long url, value is shorten url Check if it is null)</para>
+        /// </summary>
+        /// <param name="messageText">Message content</param>
+        public Tuple<bool,string,List<KeyValuePair<string,string>>> ConvertToShortenUrl(string messageText)
+        {
+            //var linkParser = new Regex(@"\b(?:https?://|www\.)\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            //var linkParser = new Regex(@"(?:(?:https?|ftp|file)::?\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            //var linkParser = new Regex(@"(?:(?:https?|ftp|file):\/\/?)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var linkParser = new Regex(@"(?:(?:https?|ftp):\/\/|\b(?:[a-z\d]+\.))(?:(?:[^\s()<>]+|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))?\))+(?:\((?:[^\s()<>]+|(?:\(?:[^\s()<>]+\)))?\)|[^\s`!()\[\]{};:'"".,<>?«»“”‘’]))?", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            //var linkParser = new Regex(@"(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'"".,<>?«»“”‘’]))");
+            //var linkParser = new Regex(@"^(https?|ftp)://[^\s/$.?#].[^\s]*$");
+            //var linkParser = new Regex(@"^(?:(http[s]?|ftp[s]):\/\/)?([^:\/\s]+)(:[0-9]+)?((?:\/\w+)*\/)([\w\-\.]+[^#?\s]+)([^#\s]*)?(#[\w\-]+)?");
+            //var linkParser = new Regex(@"[(http(s)?):\/\/?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)");
+            var matchesArray = linkParser.Matches(messageText).Cast<Match>().Where(x => culture.CompareInfo.IndexOf(x.Value, "telekurye", CompareOptions.IgnoreCase) >= 0)
+                .Select(s => s.Value).ToArray();
+            
+            var keyValueList = new List<KeyValuePair<string,string>>();
+
+            if (matchesArray.Length == 0)
+                return new Tuple<bool, string, List<KeyValuePair<string, string>>>(true, messageText, null);
+
+            foreach (var match in matchesArray)
             {
-                return new ApiResponse
-                {
-                    Ok = true,
+                var shortenUrl = Shorten(match);
 
-                    Kind = response.Data.Kind,
-                    Id = response.Data.Id,
-                    LongUrl = response.Data.LongUrl
-                    //Status = jObject.Value<string>("status")
-                };
+                if (!string.IsNullOrEmpty(shortenUrl))
+                {
+                    keyValueList.Add(new KeyValuePair<string, string>(match, shortenUrl));
+                    messageText = messageText.Replace(match, shortenUrl);
+                }
+                else
+                    return new Tuple<bool, string, List<KeyValuePair<string, string>>>(false,messageText,null);
             }
 
-            var error = response.Data.Error;
-            var errors = response.Data.Error.Errors;
-
-            return new ApiResponse
-            {
-                Ok = false,
-                Error = new Error
-                {
-                    Errors = errors.Select(x => new ErrorDescription
-                    {
-                        Domain = x.Domain,
-                        Reason = x.Reason,
-                        Message = x.Message,
-                        Location = x.Location,
-                        LocationType = x.LocationType
-                    }).ToArray(),
-                    Code = error.Code,
-                    Message = error.Message
-                }
-            };
+            return new Tuple<bool, string, List<KeyValuePair<string, string>>>(true, messageText,keyValueList);
         }
-
-        public ApiResponse Tracker(string shortUrl)
-        {
-            //https://www.googleapis.com/urlshortener/v1/url?shortUrl=https://goo.gl/L2NRzo&projection=FULL&key=AIzaSyB_CuchBvjq9C2AcbSj3r3EIIm1dAIyHIA
-
-            var uri = _uri + "?shortUrl=" + shortUrl + "&projection=FULL&key=" + _apiKey;
-            var client = new RestClient(uri);
-            var request = new RestRequest { Method = Method.GET };
-            var response = client.Execute<ApiResponse>(request);
-
-
-            if (response.StatusCode == HttpStatusCode.OK)
-            {
-                return response.Data;
-                //    new ApiResponse
-                //{
-                //    Ok = true,
-                //    Kind = response.Data.Kind,
-                //    Id = response.Data.Id,
-                //    LongUrl = response.Data.LongUrl,
-                //    Status = response.Data.Status
-                //};
-            }
-
-            var error = response.Data.Error;
-            var errors = response.Data.Error.Errors;
-
-            return new ApiResponse
-            {
-                Ok = false,
-                Error = new Error
-                {
-                    Errors = errors.Select(x => new ErrorDescription
-                    {
-                        Domain = x.Domain,
-                        Reason = x.Reason,
-                        Message = x.Message,
-                        Location = x.Location,
-                        LocationType = x.LocationType
-                    }).ToArray(),
-                    Code = error.Code,
-                    Message = error.Message
-                }
-            };
-        }
-
     }
+
+    public class ApiResponse
+    {
+        public bool Ok { get; set; }
+        public string Kind { get; set; }
+        public string Id { get; set; }
+        public string LongUrl { get; set; }
+        public string Status { get; set; }
+        public Error Error { get; set; }
+        public DateTime created { get; set; }
+        public Analytics analytics { get; set; }
+    }
+
+    public class Error
+    {
+        public ErrorDescription[] Errors { get; set; }
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class ErrorDescription
+    {
+        public string Domain { get; set; }
+        public string Reason { get; set; }
+        public string Message { get; set; }
+        public string LocationType { get; set; }
+        public string Location { get; set; }
+    }
+
+    public class Referrer
+    {
+        public string count { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Country
+    {
+        public string count { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Browser
+    {
+        public string count { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Platform
+    {
+        public string count { get; set; }
+        public string id { get; set; }
+    }
+
+    public class AllTime
+    {
+        public string shortUrlClicks { get; set; }
+        public string longUrlClicks { get; set; }
+        public List<Referrer> referrers { get; set; }
+        public List<Country> countries { get; set; }
+        public List<Browser> browsers { get; set; }
+        public List<Platform> platforms { get; set; }
+    }
+
+    public class ReferrerInMonth
+    {
+        public string count { get; set; }
+        public string id { get; set; }
+    }
+
+    public class CountryInMonth
+    {
+        public string count { get; set; }
+        public string id { get; set; }
+    }
+
+    public class BrowserInMonth
+    {
+        public string count { get; set; }
+        public string id { get; set; }
+    }
+
+    public class PlatformInMonth
+    {
+        public string count { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Month
+    {
+        public string shortUrlClicks { get; set; }
+        public string longUrlClicks { get; set; }
+        public List<ReferrerInMonth> referrers { get; set; }
+        public List<CountryInMonth> countries { get; set; }
+        public List<BrowserInMonth> browsers { get; set; }
+        public List<PlatformInMonth> platforms { get; set; }
+    }
+
+    public class ReferrerInWeek
+    {
+        public string count { get; set; }
+        public string id { get; set; }
+    }
+
+    public class CountryInWeek
+    {
+        public string count { get; set; }
+        public string id { get; set; }
+    }
+
+    public class BrowserInWeek
+    {
+        public string count { get; set; }
+        public string id { get; set; }
+    }
+
+    public class PlatformInWeek
+    {
+        public string count { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Week
+    {
+        public string shortUrlClicks { get; set; }
+        public string longUrlClicks { get; set; }
+        public List<ReferrerInWeek> referrers { get; set; }
+        public List<CountryInWeek> countries { get; set; }
+        public List<BrowserInWeek> browsers { get; set; }
+        public List<PlatformInWeek> platforms { get; set; }
+    }
+
+    public class Day
+    {
+        public string shortUrlClicks { get; set; }
+        public string longUrlClicks { get; set; }
+    }
+
+    public class TwoHours
+    {
+        public string shortUrlClicks { get; set; }
+        public string longUrlClicks { get; set; }
+    }
+
+    public class Analytics
+    {
+        public AllTime allTime { get; set; }
+        public Month month { get; set; }
+        public Week week { get; set; }
+        public Day day { get; set; }
+        public TwoHours twoHours { get; set; }
+    }
+
+
+
+
 }
