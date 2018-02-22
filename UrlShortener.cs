c007cc898a2cb66d572206cee30f914adacbad05
Murat Ay <muratay@telekurye.wan>
2018-02-22 18:40:22 +0300

    remove api key

--- UrlShortener.cs
+++ UrlShortener.cs
@@ -15,7 +15,7 @@ namespace ShortenUrl
     {
         private static UrlShortener _instance;
 
-        private const string ApiKey = "AIzaSyB_CuchBvjq9C2AcbSj3r3EIIm1dAIyHIA1";
+        private const string ApiKey = "----------------------------------------";
         
         private static readonly RestClient Client = new RestClient("https://www.googleapis.com/urlshortener/v1/url");
         private static readonly CultureInfo culture = new CultureInfo("en-US");
